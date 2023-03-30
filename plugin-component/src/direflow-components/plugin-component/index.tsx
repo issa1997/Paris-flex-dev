755934db5c4cadf92e8d7d7009edb8c3fec8e5ae
Yohan <yohanperera27@gmail.com>
2023-03-31 00:03:00 +0530

    added material ui fixed rendering issues and added card component

--- plugin-component/src/direflow-components/plugin-component/index.tsx
+++ plugin-component/src/direflow-components/plugin-component/index.tsx
@@ -1,19 +1,22 @@
-import { DireflowComponent } from 'direflow-component';
-import App from './App';
+import { DireflowComponent } from "direflow-component";
+import App from "./App";
 
 export default DireflowComponent.create({
   component: App,
   configuration: {
-    tagname: 'plugin-component',
+    tagname: "plugin-component",
   },
   plugins: [
     {
-      name: 'font-loader',
+      name: "font-loader",
       options: {
         google: {
-          families: ['Advent Pro', 'Noto Sans JP'],
+          families: ["Advent Pro", "Noto Sans JP"],
         },
       },
+    },
+    {
+      name: "material-ui",
     },
   ],
 });
