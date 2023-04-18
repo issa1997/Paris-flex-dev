a6aa4327841405d8263c5b1b3a795eaa8dca826b
Yohan <yohanperera27@gmail.com>
2023-04-18 23:38:35 +0530

    added db connection and deleted not needed resources

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,39 +4,35 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CommandModule } from 'nestjs-command';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CraftCategoriesCommand } from './utls/seed-commands/craft-category.command';
-import { CraftCategoriesModule } from './craft-categories/craft-categories.module';
 import { UserModule } from './user/user.module';
 import { UserCommand } from './utls/seed-commands/user.command';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { RolesGuard } from './auth/roles.guard';
-import { CraftsModule } from './crafts/crafts.module';
-import { CraftsCommand } from './utls/seed-commands/craft.command';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 /**
  * Usage and Description - This file will act as the main
  * app wrapper combining the controller functions and the
  * service functions
  *
  **/
-
-const developmentDBURL = `mongodb://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/`;
-
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      process.env.SERVER_ENV == 'development'
-        ? developmentDBURL
-        : process.env.MONOGO_URL,
-    ),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: true,
+    }),
     CommandModule,
-    CraftCategoriesModule,
     UserModule,
     AuthModule,
-    CraftsModule,
   ],
   controllers: [AppController],
-  providers: [AppService, CraftCategoriesCommand, UserCommand, CraftsCommand],
+  providers: [AppService, UserCommand],
 })
 export class AppModule {}
