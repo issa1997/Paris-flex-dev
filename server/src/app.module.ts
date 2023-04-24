079619feda67127364577f8490ca9f61d8a9eb1d
Yohan <yohanperera27@gmail.com>
2023-04-24 23:46:21 +0530

    added new db changes

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,15 +1,10 @@
 require('dotenv').config();
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { CommandModule } from 'nestjs-command';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { UserCommand } from './utls/seed-commands/user.command';
-import { AuthModule } from './auth/auth.module';
-import { APP_GUARD } from '@nestjs/core';
-import { RolesGuard } from './auth/roles.guard';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RatesModule } from './rates/rates.module';
 /**
  * Usage and Description - This file will act as the main
  * app wrapper combining the controller functions and the
@@ -29,10 +24,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       synchronize: true,
     }),
     CommandModule,
-    UserModule,
-    AuthModule,
+    RatesModule,
   ],
   controllers: [AppController],
-  providers: [AppService, UserCommand],
+  providers: [AppService],
 })
 export class AppModule {}
