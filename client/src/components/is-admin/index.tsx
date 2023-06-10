c8728beab38bb29a996e94de6b37cbe39b5e332f
Malish96 <malishaponweera@gmail.com>
2023-06-10 21:20:34 +0530

    Redirect to login page

--- client/src/components/is-admin/index.tsx
+++ client/src/components/is-admin/index.tsx
@@ -1,10 +1,13 @@
 import { Navigate, Outlet } from "react-router-dom";
 
 export default function IsAdmin(): JSX.Element {
+
   let loginJson = localStorage.getItem("userObject");
   let loginData = loginJson ? JSON.parse(loginJson) : null;
-  if(loginData.userRole && loginData.userRole === "ADMIN"){
+
+  if(loginData && loginData.userRole && loginData.userRole === "ADMIN"){
     return (<Outlet />);
+  }else{
+    return (<Navigate to="/login" />);
   }
-  return (<Navigate to="/" />);
 }
